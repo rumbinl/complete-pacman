a818281cc65d85f645b08f9b6fb014eb97faeb43
Rumbin Lee <rumbinlee@gmail.com>
2023-12-10 18:27:43 +0100

    MacOS rendering setup initiated

---
+++ src/mac/window.mm
@@ -0,0 +1,10 @@
+#include <Foundation/Foundation.h>
+#include <CoreGraphics/CoreGraphics.h>
+#include <Metal/Metal.h>
+
+int main()
+{
+	id<MTLDevice> device = MTLCreateSystemDefaultDevice();
+	CGContext* context;	
+	return 0;
+}
