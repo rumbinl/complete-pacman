b2899839a0ebe4f5e0b043f63f3695856a30fcce
Rumbin Lee <rumbinlee@gmail.com>
2023-12-10 22:21:44 +0100

    Created NSWindow

--- src/mac/window.mm
+++ src/mac/window.mm
@@ -1,10 +1,22 @@
 #include <Foundation/Foundation.h>
-#include <CoreGraphics/CoreGraphics.h>
+#include <Cocoa/Cocoa.h>
 #include <Metal/Metal.h>
 
 int main()
 {
-	id<MTLDevice> device = MTLCreateSystemDefaultDevice();
-	CGContext* context;	
+	//id<MTLDevice> device = MTLCreateSystemDefaultDevice();
+
+	NSRect contentRect = NSMakeRect(0, 480, 640, 480);
+	NSWindowStyleMask styleMask = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable;
+	NSBackingStoreType backingStoreType = NSBackingStoreBuffered;
+
+	NSWindow* window = [[NSWindow alloc]initWithContentRect:contentRect styleMask: styleMask backing: backingStoreType defer: true];
+	[[window contentView]initWithFrame: contentRect];
+
+	[window setBackgroundColor: [NSColor colorWithSRGBRed:1.0 green: 0.5 blue: 0.25 alpha: 0.9]];
+
+	[window makeKeyAndOrderFront: NSApp];
+	[NSApp run];
+	
 	return 0;
 }
